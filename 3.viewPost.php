99065dc528e6a765ab85bcda76bb66ad4cce519b
DESKTOP-CT3H8F2\zhiho <zhihongddt@gmail.com>
2021-11-15 10:36:53 +0800

    none

--- 3.viewPost.php
+++ 3.viewPost.php
@@ -6,6 +6,7 @@ if(isset($_GET['id'])) {
 	echo "invalid id";
 	exit;
 }
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -65,5 +66,7 @@ while (	$rs = mysqli_fetch_assoc($result)) {
       <input type="submit" name="Submit" value="送出" />
     </label></td>
 	</form>
+
+while()
 </body>
 </html>
